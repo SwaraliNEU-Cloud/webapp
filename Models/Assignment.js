9afcd73187c74432177abe199d48c030c637c2b4
swaralipatil1 <145506009+swaralipatil1@users.noreply.github.com>
2023-10-25 10:38:57 -0400

    webapp service updates

--- Models/Assignment.js
+++ Models/Assignment.js
@@ -51,37 +51,9 @@ const Assignment = sequelize.define('Assignment', {
     Assignment.belongsTo(models.User, { foreignKey: 'userId', as: 'user' });
   };
 
-
   console.log(Assignment === sequelize.models.Assignment);
 
   module.exports = Assignment
 
 
 
-// const { Sequelize, DataTypes } = require('sequelize');
-// // const { DataTypes } = require('sequelize');
-// // const sequelize = require('../config/config');
-// const sequelize = new Sequelize('swaradb', 'root', 'Sp@17111997', {
-//     host: 'localhost', // Change to your database host if needed
-//     dialect: 'mysql', // Use the appropriate dialect (e.g., 'postgres' for PostgreSQL)
-//   });
-
-// const Assignment = sequelize.define('Assignment', {
-//   title: {
-//     type: DataTypes.STRING,
-//     allowNull: false,
-//   },
-//   description: {
-//     type: DataTypes.TEXT,
-//   },
-//   points: {
-//     type: DataTypes.INTEGER,
-//     allowNull: false,
-//     validate: {
-//       min: 1,
-//       max: 10,
-//     },
-//   },
-// });
-
-// module.exports = Assignment;
