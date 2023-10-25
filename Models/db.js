02872da91dda366e0095f766f2c7eb992becca93
swaralipatil1 <145506009+swaralipatil1@users.noreply.github.com>
2023-10-25 00:48:47 -0400

    sequalize update

--- Models/db.js
+++ Models/db.js
@@ -1,41 +1,17 @@
-const Sequelize = require("sequelize");
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
+// db.js
 
-console.log("---------")
+const { Sequelize, DataTypes } = require('sequelize');
+const mysql = require('mysql2');
+require('dotenv').config(); 
 
-console.log(process.env.NODE_ENV);
+const db_address = process.env.DB_ENDPOINT.toString().split(':')
 
-console.log(process.env.DB_NAME);
-
-console.log(process.env.DB_USER);
-
-console.log(process.env.DB_PASSWORD);
-
-console.log(process.env.DB_HOST);
-
-console.log("---------")
-var port = process.env.DB_PORT;
-// require("dotenv").config(); // Load environment variables from .env
-const sequelize = new Sequelize({
-
-  database: process.env.DB_NAME,
-
-  username: process.env.DB_USER,
-
-  password: process.env.DB_PASSWORD,
-
-  host: process.env.DB_HOST,
-
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
-
-  dialect: "mysql",
-
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,{
+    dialect: 'mysql',
+    host: db_address[0],
+    port: parseInt(db_address[1]),
 });
 
- 
 
-module.exports = sequelize;
 
- 
+module.exports = sequelize
