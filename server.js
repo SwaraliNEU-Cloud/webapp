4733965eb0122c47e3c05b1083e55b471c172dfd
swaralipatil1 <145506009+swaralipatil1@users.noreply.github.com>
2023-11-05 10:29:43 -0500

    application log bug fixes for API call 1

--- server.js
+++ server.js
@@ -17,6 +17,11 @@ const logger = require('./Models/logHelper');
 const PORT = 8080;
 
 logger.info("Web App is started..")
+app.use((req, res, next) => {
+    // Log the request information
+    logger.info(`API Request: ${req.method} ${req.url}`);
+    next();
+  });
 
 Users.hasMany(Assignment, { foreignKey: 'userId', as: 'assignments' });
 
