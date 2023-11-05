de06d84ac11b1943a4e7797623015ca11b845ee4
swaralipatil1 <145506009+swaralipatil1@users.noreply.github.com>
2023-11-04 22:11:10 -0400

    Assignment7 1 (#28)
    * new feature
    
    * new changes
    
    * env environment
    
    * json fix
    
    * json fix1
    
    * sql fix
    
    * sql fix1
    
    * sql fix2
    
    * sql fix3
    
    * sql fix4
    
    * sql fix5
    
    * sql fix6
    
    * run WF
    
    * run WF add
    
    * run WF add with env
    
    * run WF add with env var
    
    * test
    
    * fixed issues
    
    * fixed issues 1
    
    * fixed issues 3
    
    * fixed issues 4
    
    * fixed issues 5
    
    * Assignment5
    
    * file formatted
    
    * bug fix
    
    * systemd
    
    * systemd_1
    
    * updated service
    
    * updated db.js
    
    * ec2 user
    
    * sequalize update
    
    * sequalize
    
    * webapp service update
    
    * webapp service update 1
    
    * webapp service update 2
    
    * webapp service update 3
    
    * webapp service update 4
    
    * webapp service update 5
    
    * webapp service update 5
    
    * Update ami-packer.pkr.hcl
    
    * webapp service updates
    
    * webapp service updates
    
    * webapp
    
    * cloudwatch updates
    
    * fixing validate of packer
    
    * git workflow fix
    
    * fixed integ tests
    
    * application log
    
    * application log bug fixes

--- Models/logHelper.js
+++ Models/logHelper.js
@@ -7,7 +7,7 @@ const logger = winston.createLogger({
   format: winston.format.simple(),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: 'var/log/application.log' }),
+    new winston.transports.File({ filename: 'application.log' }),
   ],
 });
 
