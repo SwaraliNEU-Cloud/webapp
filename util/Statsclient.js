06e73f933bffdbb82090dace93183c20bb23573c
swaralipatil1 <145506009+swaralipatil1@users.noreply.github.com>
2023-11-08 13:34:55 -0500

    packer

--- util/Statsclient.js
+++ util/Statsclient.js
@@ -1,23 +1,3 @@
-const StatsD = require('node-statsd');
-const statsdConfig = {
-    graphitePort: 2003,
-    graphiteHost: "localhost",
-    port: 8125,
-    debug: true,
-    backends: ["./backends/console", "./backends/graphite"],
-    graphite: {
-      legacyNamespace: false,
-      globalPrefix: "stats",
-      prefixCounter: "",
-      prefixTimer: "",
-      prefixGauge: "",
-      prefixSet: ""
-    },
-    flushInterval: 60000
-  };
-  
-  
-
 // File: utils/statsdClient.js
 const StatsD = require('hot-shots');
 const statsd = new StatsD({
