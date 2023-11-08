12d0ae20fd11d4960c384fe07a6f875de50abb6b
swaralipatil1 <145506009+swaralipatil1@users.noreply.github.com>
2023-11-08 11:07:55 -0500

    statsd updates 21

--- util/Statsclient.js
+++ util/Statsclient.js
@@ -18,21 +18,18 @@ const statsdConfig = {
   
   
 
-
-
-
-// // File: utils/statsdClient.js
-// const StatsD = require('hot-shots');
-// const statsd = new StatsD({
-//   host: 'localhost',
-//   port: 8125,
-//   prefix: 'your_app_prefix.',
-//   globalTags: { env: process.env.NODE_ENV },
-//   errorHandler: (error) => {
-//     console.error('StatsD error:', error);
-//   }
-// });
+// File: utils/statsdClient.js
+const StatsD = require('hot-shots');
+const statsd = new StatsD({
+  host: 'localhost',
+  port: 8125,
+  prefix: 'your_app_prefix.',
+  globalTags: { env: process.env.NODE_ENV },
+  errorHandler: (error) => {
+    console.error('StatsD error:', error);
+  }
+});
  
-// module.exports = statsd;
+module.exports = statsd;
 
  
