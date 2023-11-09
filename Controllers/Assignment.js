0e06be0d9cf2e8c82edcf3dddb18e924b9141662
swaralipatil1 <145506009+swaralipatil1@users.noreply.github.com>
2023-11-08 19:27:06 -0500

    metrices 5

--- Controllers/Assignment.js
+++ Controllers/Assignment.js
@@ -4,7 +4,7 @@ const Sequelize = require('../Models/db');
 const logger = require('../Models/logHelper');
 // Create an assignment
 exports.createAssignment = async (req, res) => {
-    statsd.increment('endpoint.hits.v1.assignment.create');  
+    // statsd.increment('endpoint.hits.v1.assignment.create');  
   try {
     const {
         name, points, num_of_attempts, deadline
@@ -12,9 +12,11 @@ exports.createAssignment = async (req, res) => {
     const userId = req.user.id;
     
     if (!name || !points || !num_of_attempts || !deadline) {
+        logger.info('Bad Request');
         return res.status(400).json({ message: 'Invalid input: All fields are required' });
     }
         if (!req.user || !req.user.id) {
+            logger.info('Unathorized');
         return res.status(401).json({ message: 'Unauthorized' });
     }
     const newAssignment = await Sequelize.models.Assignment.create({
@@ -27,6 +29,7 @@ exports.createAssignment = async (req, res) => {
     logger.info('Assignment created successfully');
     res.status(201).json({ message: 'Assignment created successfully', data: newAssignment });
 } catch (error) {
+    logger.info('Server error');
     return res.status(500).json({ message: "Server error", error: error.message });
 }
 };
